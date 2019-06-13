980f3706bd99e31be5f32ef0a8da54a022d03605
Shubham Mittal <shubhammittal5114@gmail.com>
2019-06-13 10:24:07 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+import 'theme/colors.dart';
+import 'top_bar.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CRIS App',
+      theme: appTheme,
+      home: Scaffold(
+        body: Container(
+        // child: TopBar(),
+        child: App(),
+        ),
+      ),
+    );
+  }
+}
+
+class App extends StatefulWidget {
+  @override
+  AppState createState() => AppState();
+}
+
+class AppState extends State<App> {
+  Color caughtColor = Colors.grey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        DragBox(Offset(70.0, 200.0), 'Body', Colors.blueAccent),
+        DragBox2(Offset(280.0, 280.0), 'Wheel_1', Colors.orange),
+        DragBox2(Offset(120.0, 280.0), 'Wheel_2', Colors.lightGreen),
+        Positioned(
+          left: 100.0,
+          bottom: 0.0,
+          child: DragTarget(
+            onAccept: (Color color) {
+              caughtColor = color;
+            },
+            builder: (
+              BuildContext context,
+              List<dynamic> accepted,
+              List<dynamic> rejected,
+            ) {
+              return Container(
+                width: 200.0,
+                height: 200.0,
+                decoration: BoxDecoration(
+                  color: accepted.isEmpty ? caughtColor : Colors.grey.shade200,
+                ),
+                child: Center(
+                  child: Text("Defect area"),
+                ),
+              );
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class DragBox extends StatefulWidget {
+  final Offset initPos;
+  final String label;
+  final Color itemColor;
+
+  DragBox(this.initPos, this.label, this.itemColor);
+
+  @override
+  DragBoxState createState() => DragBoxState();
+}
+
+class DragBoxState extends State<DragBox> {
+  Offset position = Offset(0.0, 0.0);
+
+  @override
+  void initState() {
+    super.initState();
+    position = widget.initPos;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        left: position.dx,
+        top: position.dy,
+        child: Draggable(
+          data: widget.itemColor,
+          child: Container(
+            width: 300.0,
+            height: 100.0,
+            color: widget.itemColor,
+            child: Center(
+              child: Text(
+                widget.label,
+                style: TextStyle(
+                  color: Colors.white,
+                  decoration: TextDecoration.none,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ),
+          onDraggableCanceled: (velocity, offset) {
+            setState(() {
+              position = offset;
+            });
+          },
+          feedback: Container(
+            width: 120.0,
+            height: 120.0,
+            color: widget.itemColor.withOpacity(0.5),
+            child: Center(
+              child: Text(
+                widget.label,
+                style: TextStyle(
+                  color: Colors.white,
+                  decoration: TextDecoration.none,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+
+// ----------------------------------------------------------------------------------------
+
+class DragBox2 extends StatefulWidget {
+  final Offset initPos;
+  final String label;
+  final Color itemColor;
+
+  DragBox2(this.initPos, this.label, this.itemColor);
+
+  @override
+  DragBoxState2 createState() => DragBoxState2();
+}
+
+class DragBoxState2 extends State<DragBox2> {
+  Offset position = Offset(0.0, 0.0);
+
+  @override
+  void initState() {
+    super.initState();
+    position = widget.initPos;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        left: position.dx,
+        top: position.dy,
+        child: Draggable(
+          data: widget.itemColor,
+          child: Container(
+            width: 60.0,
+            height: 60.0,
+            color: widget.itemColor,
+            child: Center(
+              child: Text(
+                widget.label,
+                style: TextStyle(
+                  color: Colors.white,
+                  decoration: TextDecoration.none,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ),
+          onDraggableCanceled: (velocity, offset) {
+            setState(() {
+              position = offset;
+            });
+          },
+          feedback: Container(
+            width: 120.0,
+            height: 120.0,
+            color: widget.itemColor.withOpacity(0.5),
+            child: Center(
+              child: Text(
+                widget.label,
+                style: TextStyle(
+                  color: Colors.white,
+                  decoration: TextDecoration.none,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+// -----------------------------------------------------------
+
+class _HomePageState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: TopBar(),
+      ),
+    );
+  }
+}
